cec77ceddc0dd85633c20e92f0cdbbd0f121521c
13731160065 <714318973@qq.com>
2018-09-02 02:26:39 +0800

    1

---
+++ WZZLogToolDemo/WZZLogToolDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZZLogToolDemo
+//
+//  Created by 王泽众 on 2018/9/2.
+//  Copyright © 2018年 王泽众. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZZLogToolDemo/WZZLogToolDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WZZLogToolDemo
+//
+//  Created by 王泽众 on 2018/9/2.
+//  Copyright © 2018年 王泽众. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
